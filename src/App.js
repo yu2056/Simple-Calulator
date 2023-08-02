3901272f6f9f4c05236df19209c9840bb8f4acad
Abdur Rehman <abdurrehmandev6@gmail.com>
2023-08-02 18:01:16 +0500

    calculator func completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ButtonsContainer from "./components/ButtonsContainer";
+import DisplayContainer from "./components/DisplayContainer";
+import "./styles.css";
 
 function App() {
+  const [display, setDisplay] = useState("");
+  const [result, setResult] = useState("");
+
+  function handleClick(e) {
+    const targetValue = e.target.name;
+    setDisplay(display + targetValue);
+  }
+
+  function operatorClick(operator) {
+    let lastCharacter = display.slice(-2);
+    let operatorsArray = ["+ ", "- ", "* ", "/ "];
+
+    console.log(lastCharacter);
+
+    if (display === "" || operatorsArray.includes(lastCharacter)) return;
+
+    setDisplay((prevDisplay) => {
+      return prevDisplay + " " + operator + " ";
+    });
+  }
+
+  function handleEqual() {
+    if (display.slice(-2).includes("+ ", "- ", "* ", "/ ")) return;
+
+    setDisplay("");
+
+    try {
+      const resultValue = calculate(display);
+      setResult(resultValue);
+    } catch (error) {
+      setDisplay("Error");
+    }
+  }
+
+  function calculate(expression) {
+    const tokens = expression.split(" ");
+    let resultValue = parseInt(tokens[0]);
+
+    for (let i = 1; i < tokens.length; i += 2) {
+      const operator = tokens[i];
+      const nextNumber = parseInt(tokens[i + 1]);
+
+      switch (operator) {
+        case "+":
+          resultValue += nextNumber;
+          break;
+        case "-":
+          resultValue -= nextNumber;
+          break;
+        case "*":
+          resultValue *= nextNumber;
+          break;
+        case "/":
+          resultValue /= nextNumber;
+          break;
+        default:
+          resultValue = "Error";
+      }
+    }
+    return resultValue;
+  }
+
+  function clear() {
+    setDisplay("");
+    setResult("");
+  }
+
+  function backspace() {
+    setDisplay(display.slice(0, -1));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="calculator">
+          <DisplayContainer
+            display={display}
+            result={result}
+            backspace={backspace}
+            clear={clear}
+          />
+          <ButtonsContainer
+            operatorClick={operatorClick}
+            handleClick={handleClick}
+            handleEqual={handleEqual}
+          />
+          <p className="text-white">Created by Abdur Rehman</p>
+        </div>
+      </div>
+    </>
   );
 }
 
